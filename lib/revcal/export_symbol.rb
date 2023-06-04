4879005c43676b12097b097e13c8a7cf496512b9
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2023-06-04 16:16:33 -0500

    Lowercase English descriptions

--- lib/revcal/export_symbol.rb
+++ lib/revcal/export_symbol.rb
@@ -10,6 +10,7 @@ module Revcal
       english_name, description = english_description.split(" - ")
 
       id = english_name
+        .gsub(/^the /, "")
         .gsub("’", "")
         .gsub("-", "_")
         .gsub(/( )+/, "_")
